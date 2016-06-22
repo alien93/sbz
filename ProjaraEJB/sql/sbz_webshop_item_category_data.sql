6361da90acca9318b9df90357783bd3482af158a
Nina <nina.marjanovic@gmail.com>
2016-06-22 13:39:37 +0200

    items data

---
+++ ProjaraEJB/sql/sbz_webshop_item_category_data.sql
@@ -0,0 +1,44 @@
+INSERT INTO `webshop_sbz`.`item_category`
+(`ITCAT_CODE`,
+`ITE_ITCAT_CODE`,
+`ITCAT_NAME`,
+`ITCAT_MDISCOUNT`)
+VALUES
+("1",
+null,
+"Nameštaj",
+50);
+
+INSERT INTO `webshop_sbz`.`item_category`
+(`ITCAT_CODE`,
+`ITE_ITCAT_CODE`,
+`ITCAT_NAME`,
+`ITCAT_MDISCOUNT`)
+VALUES
+("2",
+"1",
+"Dnevna soba",
+70);
+
+INSERT INTO `webshop_sbz`.`item_category`
+(`ITCAT_CODE`,
+`ITE_ITCAT_CODE`,
+`ITCAT_NAME`,
+`ITCAT_MDISCOUNT`)
+VALUES
+("3",
+"1",
+"Spavaća soba",
+40);
+
+INSERT INTO `webshop_sbz`.`item_category`
+(`ITCAT_CODE`,
+`ITE_ITCAT_CODE`,
+`ITCAT_NAME`,
+`ITCAT_MDISCOUNT`)
+VALUES
+("4",
+"1",
+"Kuhinja",
+60);
+
