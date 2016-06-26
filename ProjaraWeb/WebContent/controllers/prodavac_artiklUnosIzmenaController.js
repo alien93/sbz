107076cc0131da38cbfb6efc208fa33a08c7a98d
ckraisnikovic <ckraisnikovic@gmail.com>
2016-06-26 15:26:46 +0200

    Prikaz slike za update

--- ProjaraWeb/WebContent/controllers/prodavac_artiklUnosIzmenaController.js
+++ ProjaraWeb/WebContent/controllers/prodavac_artiklUnosIzmenaController.js
@@ -13,6 +13,16 @@ angular.module('sbzApp')
 			$scope.artikl = angular.copy(items);
 			$scope.izmena = false; //potrebno zbog disable-ovanja id polja, a i za razlikovanje add od update
 			
+			$scope.image = "";
+			$scope.format = "";
+			
+			$scope.ucitajSliku = function() {
+ 		    	var output = document.getElementById('output');
+ 		    	var txt = '.\/images\/' + $scope.artikl.info.picture;
+ 		    	console.log(txt);
+ 		    	output.src = txt;
+ 		    };
+			
 			if ($scope.artikl.info.id == "") {
 				$scope.naslovnaPoruka = "Unos novog artikla";
 				$scope.izmena = false;
@@ -20,6 +30,7 @@ angular.module('sbzApp')
 			else {
 				$scope.naslovnaPoruka = "Izmena artikla";
 				$scope.izmena = true;
+				$scope.ucitajSliku();
 			}
 				
 			console.log("Izmena " + $scope.izmena);
@@ -42,21 +53,20 @@ angular.module('sbzApp')
 			});	
 			
 			
-			
 			$scope.potvrdi = function(){
 				// TODO : rest poziv za update ili add	
+				
 				var fd = new FormData();
 		  		
-		  		fd.append("image", $scope.image);
+		  		fd.append("image", $scope.imageBin);
 		  		fd.append("format", $scope.format);
 		  		fd.append("name",$scope.artikl.info.name);
-		  		fd.append("category",$scope.artikl.category);
+		  		fd.append("category",$scope.artikl.category.info.code);
 		  		fd.append("cost",$scope.artikl.info.cost);
 		  		fd.append("inStock",$scope.artikl.info.inStock);
 		  		fd.append("minQuantity",$scope.artikl.info.minQuantity);
 		  		
 				if ($scope.izmena == false) {
-					console.log("Dodavanje...");
 					$http.post("http://localhost:8080/ProjaraWeb/rest/items/add", fd, {
 			  			withCredentials: true,
 			  	        headers: {'Content-Type': undefined },
@@ -70,27 +80,36 @@ angular.module('sbzApp')
 			  		           console.log(error);
 
 			  		  });
-					
-					$uibModalInstance.close();
 				} else {
+					fd.append("id", $scope.artikl.info.id);
 					
+					$http.post("http://localhost:8080/ProjaraWeb/rest/items/update", fd, {
+			  			withCredentials: true,
+			  	        headers: {'Content-Type': undefined },
+			  	        transformRequest: angular.identity
+			  		 })
+			  		    .success(function(response, status, headers, config) {
+			  		           console.log(response);
+
+			  		 })
+			  		    .error(function(error, status, headers, config) {
+			  		           console.log(error);
+
+			  		  });
 				}
 				
-				
-				
-//				console.log("Adresa " + "http://localhost:8080/ProjaraWeb/rest/items/" + $scope.artikl.info.id);
-//				//Azuriranje reda u tabeli artikala
-//	 			$http({
-//					method: "GET", 
-//					url : "http://localhost:8080/ProjaraWeb/rest/items/" + $scope.artikl.info.id,
-//				}).then(function(value) {
-//					console.log("dopuna");
-//					console.log(value.data);
-//					$scope.original = value.data;
-//				}, function(reason){
-//					console.log(JSON.stringify(reason));
-//				});
-//				$uibModalInstance.close();
+				console.log("Adresa " + "http://localhost:8080/ProjaraWeb/rest/items/" + $scope.artikl.info.id);
+				//Azuriranje reda u tabeli artikala
+	 			$http({
+					method: "GET", 
+					url : "http://localhost:8080/ProjaraWeb/rest/items/" + $scope.artikl.info.id,
+				}).then(function(value) {
+					console.log(value.data);
+					$scope.original = value.data;
+				}, function(reason){
+					console.log(JSON.stringify(reason));
+				});
+				$uibModalInstance.close();
 				
 			};
 			
@@ -123,13 +142,13 @@ angular.module('sbzApp')
  		     	var dataBinReader = new FileReader();
  		     	dataBinReader.onload = function(){
 	  		     	
- 		     		$scope.json.image = input.files[0];
- 		     		$scope.json.format = input.files[0].name.split('.')[1];
- 		     		console.log($scope.json);
+ 		     		$scope.imageBin = input.files[0];
+ 		     		$scope.format = input.files[0].name.split('.')[1];
  		     	};
 
  		     	dataBinReader.readAsArrayBuffer(input.files[0]); 
  		     };
+ 		     
 				
 		}
 	]);
